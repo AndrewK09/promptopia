62076ee0df85e1e99e4f3ccbda9b9324d9cfa2f9
Andrew <Andrewkn09@Gmail.com>
2023-06-23 20:54:17 -0400

    Partially handle provider

--- components/navbar/DesktopNav.jsx
+++ components/navbar/DesktopNav.jsx
@@ -1,27 +1,45 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import { signOut } from 'next-auth/react';
+import { signIn, signOut } from 'next-auth/react';
 
-const DesktopNav = () => {
+const DesktopNav = ({ isUserLoggedIn, providers }) => {
   return (
-    <div className='flex gap-3 md:gap-5'>
-      <Link href='/create-prompt' className='black_btn'>
-        Create Post
-      </Link>
+    <div className='sm:flex hidden'>
+      {isUserLoggedIn ? (
+        <div className='flex gap-3 md:gap-5'>
+          <Link href='/create-prompt' className='black_btn'>
+            Create Post
+          </Link>
 
-      <button type='button' onClick={signOut} className='outline_btn'>
-        Sign Out
-      </button>
+          <button type='button' onClick={signOut} className='outline_btn'>
+            Sign Out
+          </button>
 
-      <Link href='/profile'>
-        <Image
-          src='/assets/images/logo.svg'
-          alt='profile'
-          width={37}
-          height={37}
-          className='rounded-full'
-        />
-      </Link>
+          <Link href='/profile'>
+            <Image
+              src='/assets/images/logo.svg'
+              alt='profile'
+              width={37}
+              height={37}
+              className='rounded-full'
+            />
+          </Link>
+        </div>
+      ) : (
+        <>
+          {providers &&
+            Object.values(providers).map((provider) => (
+              <button
+                type='button'
+                key={provider.name}
+                onClick={() => signIn(provider.id)}
+                className='black_btn'
+              >
+                Sign In
+              </button>
+            ))}
+        </>
+      )}
     </div>
   );
 };
